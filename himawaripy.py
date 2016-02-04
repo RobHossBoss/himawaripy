01d7c681ae7ce47f639672733d0f734574662833
xenith <xenithorb@users.noreply.github.com>
2016-02-04 11:18:28 -0500

    Some changes for KDE compatibility...
    1. Moved the file into a directory in the user's homedir
    2. Created dir if not exist
    3. Added shebang line to top of script
    4. Commented out gnome-specific stuffs

--- himawaripy.py
+++ himawaripy.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python3 
 from json import loads
 from io import BytesIO
 from urllib.request import urlopen
 from time import strptime, strftime
-from os import system
+from os import system, makedirs
 from os.path import expanduser
-
 from PIL import Image
 
 # Configuration
@@ -18,6 +18,7 @@ level = 4
 def main():
     width = 550
     height = 550
+    save_dir = expanduser("~/.himawari")
 
     print("Updating...")
     with urlopen("http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json") as latest_json:
@@ -39,14 +40,15 @@ def main():
         
             print("Downloading tiles: {}/{} completed".format(x*level + y + 1, level*level), end="\r")
     print("\nDownloaded\n")
-
-    png.save(expanduser("~/.himawari-latest.png"), "PNG")
+    
+    makedirs( save_dir, exist_ok=True )
+    png.save( save_dir + "/himawari-latest.png", "PNG")
 
     # Because of a bug and stupid design of gsettings, see http://askubuntu.com/a/418521/388226
-    system("gsettings set org.gnome.desktop.background draw-background false \
-            && gsettings set org.gnome.desktop.background picture-uri file://"
-            + expanduser("~/.himawari-latest.png") + 
-            " && gsettings set org.gnome.desktop.background picture-options scaled")
+    #system("gsettings set org.gnome.desktop.background draw-background false \
+    #        && gsettings set org.gnome.desktop.background picture-uri file://"
+    #        + expanduser("~/.himawari-latest.png") + 
+    #        " && gsettings set org.gnome.desktop.background picture-options scaled")
 
     print("Done!\n")
     
